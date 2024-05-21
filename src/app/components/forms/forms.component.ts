1fd930f81b9e872786e28dc9c3516c571370947d
PaterDev <josepater_1130@hotmail.com>
2024-05-21 08:48:52 -0500

    feat(reactive form): validation with required and minLength

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
@@ -9,7 +10,7 @@ import {
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css',
 })
@@ -20,8 +21,8 @@ export class FormsComponent implements OnInit {
   constructor(private forBuilder: FormBuilder) {
     this.buyForm = forBuilder.group({
       typeID: ['', Validators.required],
-      numID: ['', Validators.required],
-      phone: ['', Validators.required],
+      numID: ['', [Validators.required, Validators.minLength(6)]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
     });
   }
 
@@ -32,12 +33,45 @@ export class FormsComponent implements OnInit {
     });
   }
 
+  // Submit
   ordenar() {
+    this.focusCampos();
     if (this.buyForm.valid) {
       alert('Orden realizada con éxito!!!');
       this.buyForm.reset();
-    } else {
-      alert('Campos faltantes');
+    }
+  }
+
+  // Verificación de datos inválidos en los campos
+  hasErrors(field: string, typeError: string) {
+    return (
+      this.buyForm.get(field)?.hasError(typeError) &&
+      this.buyForm.get(field)?.touched // Si fue tocado
+    );
+  }
+
+  // Detecta si se ha tocado el campo (touched)
+  tocarCampo(field: string) {
+    return this.buyForm.get(field)?.touched;
+  }
+
+  // Hace foco en los campos inválidos o vacíos
+  focusCampos() {
+    const form = Object.keys(this.buyForm.value); // Objeto del formulario (inputs)
+
+    for (let index = 0; index < form.length; index++) {
+      const field = form[index]; // Input
+      const label = document.querySelector(`label[for="${field}"]`); // Label del input
+
+      if (this.buyForm.controls[field].status === 'INVALID') {
+        // Si el input es inválido
+        const fieldElement = window.document.getElementById(field);
+        alert(
+          `Por favor, ${label?.textContent?.replace(':', ',')} correctamente.`
+        );
+        fieldElement?.focus();
+        break;
+      }
     }
   }
 }
